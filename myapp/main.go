a57b364392a8e8dd6f96591478ed6de28b71f5d4
JunJun <928595201@qq.com>
2023-12-25 02:39:55 +0000

    "skeleton"

--- myapp/main.go
+++ myapp/main.go
@@ -5,9 +5,63 @@ import (
     "fmt"
     "io/ioutil"
     "strings"
+    // "worker"
+    // "psi/execute"
     // "github.com/rocketlaunchr/dataframe-go"
 	// "github.com/rocketlaunchr/dataframe-go/imports"
 )
+// type Context interface{
+    
+// }
+// how to implement Context interface?
+type Column struct{
+    name string
+    idx int32
+}
+type Arguments struct{
+    tag int32
+    columns []Column
+}
+type Result struct{
+    res bool
+
+}
+type DataSetReport struct {
+    id string
+    tag uint32
+    columns []string
+    row uint32
+    missing uint32
+}
+
+type PSIReport struct {
+    report_map map[string]uint32
+    dataset []DataSetReport
+}
+
+func psi_validate_args(args *Arguments) bool { //Result...
+    
+    // how to add elements in Arguments slice?
+    return true
+}
+func psi_add_dataset( args *Arguments, report *PSIReport) bool {//ctx *Context, args *Arguments, report *PSIReport
+    // get the dataset from outside: using the arguments: id, datasetid
+    psi
+    return true
+}
+func psi_read_data(args * Arguments, report * PSIReport)
+
+func psi_summary() bool{
+    return true
+}
+
+func test_PSI(){
+    // args := Arguments 
+
+    for i in range 0,3 {
+        // psi_add_dataset();
+    }
+}
 
 // func test_dataframe(){
 //     csvStr := `Country,Population,Year
@@ -29,7 +83,6 @@ import (
 //         // 打印数据帧
 //         fmt.Println(df.Table())
 // }
-
 func test(m map[string]uint8, block_path string) {
     counts := make(map[string]int)
     block_data, err := ioutil.ReadFile(block_path)
@@ -69,12 +122,12 @@ func main() {
     block1_1_path := "testdata/d1_block1.txt"
     block2_1_path := "testdata/d2_block1.txt"
     myMap := make(map[string]uint8)
+    fmt.Println(block1_1_path,block2_1_path,myMap)
+    // test(myMap, block1_1_path)
+    // fmt.Println("After processing 1:", myMap)
 
-    test(myMap, block1_1_path)
-    fmt.Println("After processing 1:", myMap)
-
-    test(myMap, block2_1_path)
-    fmt.Println("After processing 2:", myMap)
+    // test(myMap, block2_1_path)
+    // fmt.Println("After processing 2:", myMap)
 
     // test_dataframe()
 }
